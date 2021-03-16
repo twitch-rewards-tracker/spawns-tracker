66d5f855eea24d8cf74a937c294d29825006b506
chowder <andrewcxw@gmail.com>
2021-03-16 21:02:03 +0000

    Remove unused import

--- src/index.js
+++ src/index.js
@@ -1,6 +1,5 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import { Helmet } from 'react-helmet';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
